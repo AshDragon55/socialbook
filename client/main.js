35efcdbea2f54837610fcf700f66bf1c4c968aa5
AshDragon55 <ashert1000@gmail.com>
2022-05-03 14:34:49 -0500

    First commit

--- client/main.js
+++ client/main.js
@@ -7,7 +7,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 Template.nav.events({
   'click.js-add'(){
-    console.log("adding");
+    $("#addModal").modal("show");
+    
+  }
+});
+
+Template.main.events({
+  'click .js-saveProfile'() {
+    let pic = $("#profPic").val();
+    let fname = $("#first").val();
+    socialdb.insert({
+      "picPath": pic,
+      "fname": fname,
+      "createdOn": new Date().getTime()
+    });
+    $("#addModal").modal("hide");
+  }
+});
+
+Template.profile.helpers({
+  profiles() {
+    return socialdb.find();
   }
 });
 
